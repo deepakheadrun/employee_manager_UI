64afb5a6e69264b1d1e04bd264b93c74a8349525
Deepak <deepak@headrun.com>
2020-09-07 10:50:15 +0530

    Interested area modal added

--- src/components/BasicInfo.vue
+++ src/components/BasicInfo.vue
@@ -1,82 +1,12 @@
 <template>
   <div>
-     <BasicInfoModal v-on:updateBasicInfo="updateBasicInfo($event)"
-     v-bind:info="{first_name:this.f_name,last_name:this.l_name,email:this.email}"/>
-    
-    <WorkModal v-on:updateWork="updateWork($event)" 
-    v-bind:userdata="this.userdata"/>
+    <BasicInfoModal v-on:updateBasicInfo="updateBasicInfo($event)" v-bind:info="{first_name:this.f_name,last_name:this.l_name,email:this.email}" />
+    <WorkModal v-on:updateWork="updateWork($event)" v-bind:userdata="this.userdata" />
+    <CareerGoalModal v-on:updateCareerGoal="updateCareerGoal($event)" v-bind:userdata="this.userdata" />
+    <InterestedAreaModal v-on:updateInterestedArea="updateInterestedArea($event)" v-bind:interested_area="this.interested_area_list" v-bind:userId="this.userdata.user_id" />
+   
     <div class="bg-white px-4 text-left rounded">
       <span class="text-2xl pt-2">Basic Info</span>
-     
-      
-      <CareerGoalModal v-on:updateCareerGoal="updateCareerGoal($event)"
-        v-bind:userdata="this.userdata" />
-      <modal :height="300" name="interestedArea-modal">
-        <div class="p-8">
-          <h2
-            class="mt-2 text-center text-3xl leading-9 font-extrabold text-gray-900"
-          >Update Interested Area</h2>
-          <div class>
-            <div class="flex px-8 pt-4">
-              <div class="w-1/2">
-                <div class="-mt-px pt-2">
-                  <label
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                    for="interested_area"
-                  >Add New Interested Area</label>
-                  <input
-                    aria-label="Add Interested Area"
-                    v-model="new_interested_area"
-                    name="interested_area"
-                    type="text"
-                    required
-                    class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-                    placeholder="Add Interested Area"
-                  />
-                </div>
-                <div class="mt-6 pb-8">
-                  <button
-                    v-on:click="addNewInterestedArea"
-                    class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
-                  >Add</button>
-                </div>
-              </div>
-              <div class="w-1/2">
-                <div class="px-8 pt-4 pb-4">
-                  <div class="pt-4 pb-2">
-                    <span
-                      v-for="item in this.interested_area"
-                      :key="item.id"
-                      class="inline-block bg-blue-200 rounded-full mx-1 px-3 py-1 text-sm font-semibold text-gray-700 mb-2"
-                    >
-                      <button v-on:click="deleteInterestedArea(item.id)">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="100%"
-                          height="100%"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="feather feather-x cursor-pointer hover:text-yellow-400 rounded-full w-4 h-4 ml-2"
-                        >
-                          <line x1="18" y1="6" x2="6" y2="18" />
-                          <line x1="6" y1="6" x2="18" y2="18" />
-                        </svg>
-                      </button>
-                      {{item.name}}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="mt-6"></div>
-        </div>
-      </modal>
-
       <button v-if="isEditable" v-on:click="basicinfoshow" class="h-6 w-6 pt-1 mx-4">
         <svg viewBox="0 0 20 20" fill="currentColor" class="pencil-alt w-6 h-6">
           <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
@@ -195,7 +125,7 @@
       <div class="px-8 pt-4 pb-4">
         <div class="pt-4 pb-2">
           <span
-            v-for="item in this.interested_area"
+            v-for="item in this.interested_area_list"
             :key="item.id"
             class="inline-block bg-blue-200 rounded-full mx-1 px-3 py-1 text-sm font-semibold text-gray-700 mb-2"
           >{{item.name}}</span>
@@ -206,18 +136,17 @@
 </template>
 
 <script>
-
-
-import InterestedAreaService from "../services/api/InterestedAreaService";
-import BasicInfoModal from "./modals/BasicinfoModal"
-import WorkModal from "./modals/WorkModal"
-import CareerGoalModal from "./modals/CareerGoalModal"
+import BasicInfoModal from "./modals/BasicinfoModal";
+import WorkModal from "./modals/WorkModal";
+import CareerGoalModal from "./modals/CareerGoalModal";
+import InterestedAreaModal from "./modals/InterestedAreaModal";
 export default {
   name: "BasicInfo",
   components: {
-   WorkModal,
+    WorkModal,
     BasicInfoModal,
-    CareerGoalModal
+    CareerGoalModal,
+    InterestedAreaModal,
   },
   data() {
     return {
@@ -232,70 +161,49 @@ export default {
       fName: this.f_name,
       lName: this.l_name,
       email_: this.email,
-      new_interested_area: null,
+      interested_area_list: this.interested_area,
     };
   },
   methods: {
-    
-    
     basicinfoshow() {
       this.$modal.show("basicinfo-modal");
     },
-   
+
     workshow() {
       this.$modal.show("work-modal");
     },
-   
+
     goalshow() {
       this.$modal.show("goal-modal");
     },
-   
+
     interestedAreashow() {
       this.$modal.show("interestedArea-modal");
     },
-    interestedAreahide() {
-      this.$modal.hide("interestedArea-modal");
+    
+    updateInterestedArea(response) {
+      this.interested_area_list = [...response];
     },
-    addNewInterestedArea() {
-      var bodyFormData = new FormData();
 
-      bodyFormData.set("user_id", this.userdata.user_id);
-      bodyFormData.set("name", this.new_interested_area);
-      InterestedAreaService.postInterestedArea(bodyFormData)
-      .then((response) => {
-        this.interested_area.push(response);
-      });
-      this.new_interested_area = null;
-    },
-    deleteInterestedArea(id) {
-      InterestedAreaService.deleteInterestedArea(id)
-        .then(() => {
-          this.interested_area = [
-            ...this.interested_area.filter((x) => x.id != id),
-          ];
-        });
-      this.new_interested_area = null;
-    },
     updateBasicInfo(response) {
-      
-        this.$store.dispatch("setLogedInUserRole", response);
-        this.email_ = response.email;
-        this.fName = response.first_name;
-        this.lName = response.last_name;
-        this.$emit("updateBasicInfo", response);
-  
+      this.$store.dispatch("setLogedInUserRole", response);
+      this.email_ = response.email;
+      this.fName = response.first_name;
+      this.lName = response.last_name;
+      this.$emit("updateBasicInfo", response);
     },
-    updateWork(response) { 
-      this.employee_id=response.employee_id,
-      this.job_title=response.job_title,
-      this.department=response.department,
-      this.employee_type=response.employee_type,
-      this.mobile=response.mobile,
-      this.reporting_to=response.reporting_to,
-      this.date_of_joining=response.date_of_joining, response;   
+    updateWork(response) {
+      (this.employee_id = response.employee_id),
+        (this.job_title = response.job_title),
+        (this.department = response.department),
+        (this.employee_type = response.employee_type),
+        (this.mobile = response.mobile),
+        (this.reporting_to = response.reporting_to),
+        (this.date_of_joining = response.date_of_joining),
+        response;
     },
     updateCareerGoal(response) {
-    this.career_goal = response.career_goal
+      this.career_goal = response.career_goal;
     },
   },
   props: [
---
+++ src/components/modals/InterestedAreaModal.vue
@@ -0,0 +1,108 @@
+<template>
+  <modal :height="300" name="interestedArea-modal">
+    <div class="p-8">
+      <h2
+        class="mt-2 text-center text-3xl leading-9 font-extrabold text-gray-900"
+      >Update Interested Area</h2>
+      <div class>
+        <div class="flex px-8 pt-4">
+          <div class="w-1/2">
+            <div class="-mt-px pt-2">
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2"
+                for="interested_area"
+              >Add New Interested Area</label>
+              <input
+                aria-label="Add Interested Area"
+                v-model="new_interested_area"
+                name="interested_area"
+                type="text"
+                required
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Add Interested Area"
+              />
+            </div>
+            <div class="mt-6 pb-8">
+              <button
+                v-on:click="addNewInterestedArea"
+                class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+              >Add</button>
+            </div>
+          </div>
+          <div class="w-1/2">
+            <div class="px-8 pt-4 pb-4">
+              <div class="pt-4 pb-2">
+                <span
+                  v-for="item in this.interested_area_list"
+                  :key="item.id"
+                  class="inline-block bg-blue-200 rounded-full mx-1 px-3 py-1 text-sm font-semibold text-gray-700 mb-2"
+                >
+                  <button v-on:click="deleteInterestedArea(item.id)">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="100%"
+                      height="100%"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-x cursor-pointer hover:text-yellow-400 rounded-full w-4 h-4 ml-2"
+                    >
+                      <line x1="18" y1="6" x2="6" y2="18" />
+                      <line x1="6" y1="6" x2="18" y2="18" />
+                    </svg>
+                  </button>
+                  {{item.name}}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mt-6"></div>
+    </div>
+  </modal>
+</template>
+<script>
+import InterestedAreaService from "../../services/api/InterestedAreaService";
+export default {
+  name: "InterestedAreaModal",
+  data() {
+    return {
+      new_interested_area: null,
+      interested_area_list: this.interested_area,
+    };
+  },
+  props: ["interested_area", "userId"],
+  methods: {
+    interestedAreahide() {
+      this.$modal.hide("interestedArea-modal");
+    },
+    addNewInterestedArea() {
+      var bodyFormData = new FormData();
+
+      bodyFormData.set("user_id", this.userId);
+      bodyFormData.set("name", this.new_interested_area);
+      InterestedAreaService.postInterestedArea(bodyFormData).then(
+        (response) => {
+          this.interested_area_list.push(response);
+          this.new_interested_area = null;
+          this.$emit("updateInterestedArea", this.interested_area_list);
+        }
+      );
+    },
+    deleteInterestedArea(id) {
+      InterestedAreaService.deleteInterestedArea(id).then(() => {
+        this.interested_area_list = [
+          ...this.interested_area_list.filter((x) => x.id != id),
+        ];
+
+        this.new_interested_area = null;
+        this.$emit("updateInterestedArea", this.interested_area_list);
+      });
+    },
+  },
+};
+</script>
