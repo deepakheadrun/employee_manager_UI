cf138f8aefc6974d936b5b7e956ce9d014368ec4
Deepak <deepak@headrun.com>
2020-09-11 18:18:29 +0530

    Landing page added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <div v-if="users.length!=0" class="min-h-screen bg-fixed bg-cover">
+<div>
+  <Landing v-if="!this.$store.state.logedInUser"/>
+  <div v-if="users.length!=0 && this.$store.state.logedInUser" class="min-h-screen bg-fixed bg-cover">
     <div class="flex flex-row justify-center mt-2" v-for="(item, i) in users" :key="i">
       <UserCard v-if="item.length >= 1" class="w-1/3 mx-4" v-bind:userinfo="item[0]" />
       <UserCard v-if="item.length >= 2" class="w-1/3 mx-4" v-bind:userinfo="item[1]" />
@@ -7,13 +9,14 @@
       <UserCard v-if="item.length >= 4" class="w-1/3 mx-4" v-bind:userinfo="item[3]" />
     </div>
   </div>
+</div>
 </template>
 
  
 <script>
 import UserCard from "./UserCard.vue";
 import UserService from "./../services/api/UserService"
-
+import Landing from "./Landing"
 export default {
   name: "Home",
   data() {
@@ -23,10 +26,12 @@ export default {
   },
   components: {
     UserCard,
+    Landing
   },
   mounted() {
     UserService.getUsers().then((response)=> 
     {
+      if(this.$store.state.logedInUser){
       let filteredUsers = response.filter((el) => {
         return el.id !== this.$store.state.logedInUser.pk;
         });
@@ -36,7 +41,7 @@ export default {
           .fill()
           .map(() => filteredUsers.splice(0, n));
         this.users.push(...result);
-      })
+      }})
     }
 };
 </script>
---
+++ src/components/Landing.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="py-40" style="background: linear-gradient(90deg, #667eea 0%, #764ba2 100%)"
+>
+  <div class="container mx-auto px-6">
+    <h2 class="text-4xl font-bold mb-2 text-white">
+      Grow With Headrunners 
+    </h2>
+    <h3 class="text-2xl mb-8 text-gray-200">
+      Monitor your career growth smartly.
+    </h3>
+ <router-link v-bind:to="'/login'" class="bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider">
+    
+      LogIn
+    
+ </router-link>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    name:'Landing'
+}
+</script>
